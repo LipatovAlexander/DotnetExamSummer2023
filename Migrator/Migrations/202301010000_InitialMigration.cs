e6d58d01373bc3205852dda2693a33136a1813cb
LipatovAlexander <lipatov.work@bk.ru>
2023-06-15 13:38:34 +0300

    feat: Add DB

---
+++ Migrator/Migrations/202301010000_InitialMigration.cs
@@ -0,0 +1,12 @@
+﻿namespace Migrator.Migrations;
+
+[TimestampedMigration(2023, 1, 1, 0, 0)]
+public sealed class InitialMigration : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Items")
+            .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+            .WithColumn("Name").AsString().NotNullable();
+    }
+}
